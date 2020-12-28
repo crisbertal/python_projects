7c60bc324142d00a4348856e95eddceeb6095f67
Cristobal Bergillos <cristobalbergillostal@gmail.com>
2020-12-28 22:18:00 +0100

    Food created

--- snake.py
+++ snake.py
@@ -1,11 +1,14 @@
 import pygame
+import random
 
 '''
 GAME SETUP 
 ''' 
 # colors
 black = (0,0,0)
+white = (255,255,255)
 red = (255,0,0)
+blue = (0,0,255)
 
 # screen
 SCREEN_WIDTH = 640
@@ -14,10 +17,27 @@ SCREEN_HEIGHT = 480
 # game vars
 x, y = SCREEN_WIDTH/2, SCREEN_HEIGHT/2
 x_lead, y_lead = 0, 0
+block_size = 20 
+
+x_food = random.randrange(0, SCREEN_WIDTH, 20) 
+y_food = random.randrange(0, SCREEN_HEIGHT, 20)
 
 # functions setup
 pygame.init()
 display = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# tickrate
+clock = pygame.time.Clock()
+
+'''
+FUNCTIONS
+'''
+def draw_grid():
+    for x in range(SCREEN_WIDTH):
+        for y in range(SCREEN_HEIGHT):
+            rect = pygame.Rect(x*block_size, y*block_size,
+                               block_size, block_size)
+            pygame.draw.rect(display, white, rect, 1)
 
 '''
 GAME LOOP
@@ -28,20 +48,25 @@ while True:
             quit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                x_lead = -10 
+                x_lead = -20 
                 y_lead = 0
             elif event.key == pygame.K_RIGHT:
-                x_lead = 10 
+                x_lead = 20 
                 y_lead = 0
             elif event.key == pygame.K_UP:
                 x_lead = 0
-                y_lead = -10
+                y_lead = -20
             elif event.key == pygame.K_DOWN:
                 x_lead = 0 
-                y_lead = 10
+                y_lead = 20
+    
+    # go out of bounds
+    if x > SCREEN_WIDTH or x < 0 or y > SCREEN_HEIGHT or y < 0:
+        x, y = SCREEN_WIDTH/2, SCREEN_HEIGHT/2
+        x_lead, y_lead = 0, 0
 
-    # tickrate
-    clock = pygame.time.Clock()
+    
+
     clock.tick(30)
 
     # movement
@@ -50,5 +75,7 @@ while True:
     
     # drawing
     display.fill(black)
-    pygame.draw.rect(display, red, (x, y, 20, 20))
+    draw_grid()
+    pygame.draw.rect(display, red, (x, y, block_size, block_size))
+    pygame.draw.rect(display, blue, (x_food, y_food, block_size, block_size))
     pygame.display.update()
